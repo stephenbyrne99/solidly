f0f6d80c9160bd5ea643454f67dd2c80eb1c65ad
Andre Cronje <andre.cronje@yearn.finance>
2022-01-02 08:59:58 +0200

    factory +feeTo
    Signed-off-by: Andre Cronje <andre.cronje@yearn.finance>

--- StableV1-core.sol
+++ StableV1-core.sol
@@ -325,11 +325,17 @@ contract StableV1Pair {
 }
 
 contract StableV1Factory {
+    address public feeTo;
+    address public feeToSetter;
 
     mapping(address => mapping(address => address)) public getPair;
     address[] public allPairs;
 
     event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+
+    constructor(address _feeToSetter) {
+        feeToSetter = _feeToSetter;
+    }
 
     function allPairsLength() external view returns (uint) {
         return allPairs.length;
@@ -354,6 +360,16 @@ contract StableV1Factory {
         getPair[token1][token0] = pair; // populate mapping in the reverse direction
         allPairs.push(pair);
         emit PairCreated(token0, token1, pair, allPairs.length);
+    }
+
+    function setFeeTo(address _feeTo) external {
+        require(msg.sender == feeToSetter, 'StableV1: FORBIDDEN');
+        feeTo = _feeTo;
+    }
+
+    function setFeeToSetter(address _feeToSetter) external {
+        require(msg.sender == feeToSetter, 'StableV1: FORBIDDEN');
+        feeToSetter = _feeToSetter;
     }
 }
 
