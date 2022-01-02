be96b0f4251c1ab0ebee7c01ac1395efe49f896d
Andre Cronje <andre.cronje@yearn.finance>
2022-01-02 08:01:22 +0200

    feat: +permit
    Signed-off-by: Andre Cronje <andre.cronje@yearn.finance>

--- StableV1-core.sol
+++ StableV1-core.sol
@@ -56,6 +56,11 @@ contract StableV1Pair {
     
     mapping(address => mapping (address => uint)) public allowance;
     mapping(address => uint) public balanceOf;
+
+    bytes32 public DOMAIN_SEPARATOR;
+    // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+    bytes32 public constant PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
+    mapping(address => uint) public nonces;
     
     event Transfer(address indexed from, address indexed to, uint amount);
     event Approval(address indexed owner, address indexed spender, uint amount);
@@ -113,6 +118,19 @@ contract StableV1Pair {
 
     constructor() {
         factory = msg.sender;
+        uint chainId;
+        assembly {
+            chainId := chainid()
+        }
+        DOMAIN_SEPARATOR = keccak256(
+            abi.encode(
+                keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)'),
+                keccak256(bytes(name)),
+                keccak256(bytes('1')),
+                chainId,
+                address(this)
+            )
+        );
     }
 
     // called once by the factory at time of deployment
@@ -260,6 +278,22 @@ contract StableV1Pair {
 
         emit Approval(msg.sender, spender, amount);
         return true;
+    }
+
+    function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external {
+        require(deadline >= block.timestamp, 'StableV1: EXPIRED');
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                '\x19\x01',
+                DOMAIN_SEPARATOR,
+                keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, nonces[owner]++, deadline))
+            )
+        );
+        address recoveredAddress = ecrecover(digest, v, r, s);
+        require(recoveredAddress != address(0) && recoveredAddress == owner, 'StableV1: INVALID_SIGNATURE');
+        allowance[owner][spender] = value;
+
+        emit Approval(owner, spender, value);
     }
 
     function transfer(address dst, uint amount) external returns (bool) {
