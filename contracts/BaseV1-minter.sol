987dc07cbb59c89dbbc2d6de4fc7d1d516a3f3e6
Andre Cronje <andre.cronje@yearn.finance>
2022-01-06 05:25:19 +0200

    Add % growth for ve stakers
    Signed-off-by: Andre Cronje <andre.cronje@yearn.finance>

--- contracts/BaseV1-minter.sol
+++ contracts/BaseV1-minter.sol
@@ -23,6 +23,11 @@ interface gauge_proxy {
     function distribute() external;
 }
 
+interface ve_dist {
+    function checkpoint_token() external;
+    function checkpoint_total_supply() external;
+}
+
 contract minter {
     uint constant week = 86400 * 7;
     uint constant emission = 4;
@@ -30,14 +35,22 @@ contract minter {
     token public immutable _token;
     gauge_proxy public immutable _gauge_proxy;
     ve public immutable _ve;
+    ve_dist public immutable _ve_dist;
     uint public available;
     uint public active_period;
 
-    constructor(address __token, uint _available, address __gauge_proxy, address  __ve) {
+    constructor(
+      address __token,
+      uint _available,
+      address __gauge_proxy,
+      address  __ve,
+      address __ve_dist
+    ) {
         _token = token(__token);
         available = _available;
         _gauge_proxy = gauge_proxy(__gauge_proxy);
         _ve = ve(__ve);
+        _ve_dist = ve_dist(__ve_dist);
     }
 
     function circulating_supply() public view returns (uint) {
@@ -56,6 +69,10 @@ contract minter {
         return circulating_supply() * emission / base;
     }
 
+    function calculate_growth(uint _minted) public view returns (uint) {
+        return _ve.totalSupply() * _minted / circulating_supply();
+    }
+
     function update_period() external returns (uint) {
         uint _period = active_period;
         if (block.timestamp >= _period + week) {
@@ -67,6 +84,9 @@ contract minter {
             }
 
             _token.mint(address(_gauge_proxy), _amount);
+            _token.mint(address(_ve_dist), calculate_growth(_amount));
+            _ve_dist.checkpoint_token();
+            _ve_dist.checkpoint_total_supply();
         }
         return _period;
     }
